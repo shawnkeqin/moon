d4cb1726ca42a41a579082af629a730c4abf4717
Shawn <e0052482@u.nus.edu>
2021-10-19 00:59:15 +0800

    redux toolkit + fetch api

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,40 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '20730420dbmsh28abee5c5b29b4bp1445a8jsn4dd93beb48c1'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'crypto',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery
+} = cryptoApi;
+// var axios = require("axios").default;
+
+// var options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/stats',
+//   headers: {
+//     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//     'x-rapidapi-key': '20730420dbmsh28abee5c5b29b4bp1445a8jsn4dd93beb48c1'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
